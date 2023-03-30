1b6f7304dca72d75fb34215a7cd66e0421be10ab
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-03-30 19:31:05 -0300

    Format code with blue

--- mytoolbox/normalize_table_columns.py
+++ mytoolbox/normalize_table_columns.py
@@ -3,12 +3,11 @@ import re
 
 
 def snake_small_case(column):
-	"""
-	"""
-	column_lower = column.lower()
-	column_unidecode = unidecode(column_lower)
-	column_alphanumeric = re.sub('[^A-Za-z0-9]+', ' ', column_unidecode)
-	column_split = column_alphanumeric.split(' ')
-	column_empty = [x for x in column_split if x != '']
-	column = '_'.join(column_empty)
-	return column	
+    """ """
+    column_lower = column.lower()
+    column_unidecode = unidecode(column_lower)
+    column_alphanumeric = re.sub('[^A-Za-z0-9]+', ' ', column_unidecode)
+    column_split = column_alphanumeric.split(' ')
+    column_empty = [x for x in column_split if x != '']
+    column = '_'.join(column_empty)
+    return column
--- tests/test_normalize_table_columns.py
+++ tests/test_normalize_table_columns.py
@@ -1,14 +1,18 @@
 import pytest
 from mytoolbox.normalize_table_columns import snake_small_case
 
-@pytest.mark.parametrize("input, output", [
-    ('My Column', 'my_column'),
-    ('MY_COLUMN', 'my_column'),
-    ('My/COLUMN_ÇAU', 'my_column_cau'),
-    ('my_column', 'my_column'),
-    ('my?column*', 'my_column'),
-    ('my-column', 'my_column'),
-    ('my_**column()', 'my_column'),
-])
+
+@pytest.mark.parametrize(
+    'input, output',
+    [
+        ('My Column', 'my_column'),
+        ('MY_COLUMN', 'my_column'),
+        ('My/COLUMN_ÇAU', 'my_column_cau'),
+        ('my_column', 'my_column'),
+        ('my?column*', 'my_column'),
+        ('my-column', 'my_column'),
+        ('my_**column()', 'my_column'),
+    ],
+)
 def test_snake_small_case(input, output):
-	assert snake_small_case(input) == output
+    assert snake_small_case(input) == output
