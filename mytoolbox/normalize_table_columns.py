092653d9a2b1f4d322a90b6cf4088c86dba882d0
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-03-30 17:49:36 -0300

    Improve snake case function

--- mytoolbox/normalize_table_columns.py
+++ mytoolbox/normalize_table_columns.py
@@ -1,6 +1,7 @@
 from unidecode import unidecode
 import re
 
+
 def snake_small_case(column):
 	column_lower = column.lower()
 	column_unidecode = unidecode(column_lower)
@@ -8,8 +9,4 @@ def snake_small_case(column):
 	column_split = column_alphanumeric.split(' ')
 	column_empty = [x for x in column_split if x != '']
 	column = '_'.join(column_empty)
-	return column
-	
-
-
-	
+	return column	
